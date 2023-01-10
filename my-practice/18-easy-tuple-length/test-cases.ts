9b25d564242f2be097fb49285ba2b30fc0f322e5
yan.hou <yan.hou@eslink.com>
2023-01-10 10:56:57 +0800

    feat: 元组长度增加一些注释

--- my-practice/18-easy-tuple-length/test-cases.ts
+++ my-practice/18-easy-tuple-length/test-cases.ts
@@ -4,12 +4,16 @@
  * @Autor: houyan
  * @Date: 2023-01-10 10:49:06
  * @LastEditors: houyan
- * @LastEditTime: 2023-01-10 10:49:12
+ * @LastEditTime: 2023-01-10 10:56:31
  */
 import type { Equal, Expect } from '@type-challenges/utils'
 
 const tesla = ['tesla', 'model 3', 'model X', 'model Y'] as const
 const spaceX = ['FALCON 9', 'FALCON HEAVY', 'DRAGON', 'STARSHIP', 'HUMAN SPACEFLIGHT'] as const
+
+
+// type a = typeof tesla
+// let b: a = ['tesla', 'model 3', 'model X', 'model Y']   // b只能是['tesla', 'model 3', 'model X', 'model Y']  没有其它值
 
 type cases = [
   Expect<Equal<Length<typeof tesla>, 4>>,
